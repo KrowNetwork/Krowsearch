de488c1b938c29938b50940dcc94f7c809c8b913
unknown <siegel.tucker@gmail.com>
2018-05-13 17:04:14 -0400

    add base files

---
+++ process.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import logging, gensim
+from gensim import similarities, corpora
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+df = pd.read_csv('data.csv')
+documents = df['description']
+titles = df['title']
+companies = list(df['company'].values)
+stoplist = set('for a of the and to in at •'.split())
+texts = [[word for word in document.lower().split() if word not in stoplist]
+         for document in documents]
+
+titles_ = [[word for word in title.lower().split() if word not in stoplist]
+         for title in titles]
+
+comapnies_ = [[word for word in company.lower().split() if word not in stoplist]
+         for company in companies]
+
+titles = titles_
+companies = comapnies_
+count = 0
+for i in texts:
+    i.extend(titles[count])
+    i.extend(companies[count])
+    count += 1
+# texts.extend(title)
+# print (companies)
+# texts.extend([companies])
+# texts[0].extend(companies)
+# remove words that appear only once
+# from collections import defaultdict
+# frequency = defaultdict(int)
+# for text in texts:
+#     for token in text:
+#         frequency[token] += 1
+#
+# texts = [[token for token in text if frequency[token] > 1]
+#          for text in texts]
+print (texts)
+# dictionary = corpora.Dictionary(texts)
+# dictionary.save('test.dict')
+# corpus = [dictionary.doc2bow(text) for text in texts]
+# corpora.MmCorpus.serialize('test.mm', corpus)
+
+dictionary = corpora.Dictionary(texts)
+dictionary.save('test.dict')
+corpus = [dictionary.doc2bow(text) for text in texts]
+corpora.MmCorpus.serialize('test.mm', corpus)
---
+++ test.py
@@ -0,0 +1,26 @@
+import logging, gensim
+from gensim import similarities, corpora, models
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+dictionary = corpora.Dictionary.load('test.dict')
+corpus = corpora.MmCorpus('test.mm') # comes from the first tutorial, "From strings to vectors"
+print(corpus)
+
+# lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=10)
+lsi = models.LdaModel(corpus, id2word=dictionary, num_topics=120)
+doc = "computer science"
+vec_bow = dictionary.doc2bow(doc.lower().split())
+vec_lsi = lsi[vec_bow]
+
+index = similarities.MatrixSimilarity(lsi[corpus])
+
+sims = index[vec_lsi]
+sims = sorted(enumerate(sims), key=lambda item: -item[1])
+print (sims)
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('data.csv')
+for i in sims[:10]:
+    print (df.ix[i[0]])
+    print ("")
