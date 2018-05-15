7820c5669c19eb59ff22e2c9434c4279b022d0bc
unknown <siegel.tucker@gmail.com>
2018-05-15 19:41:39 -0400

    cleanup

--- search.py
+++ search.py
@@ -9,24 +9,16 @@ parser.add_argument('--title',
                     help='job title to search for', default=None)
 args = parser.parse_args()
 
-# print (args.title)
-
-
 df = pd.read_csv('data job posts.csv')
 # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 dictionary = corpora.Dictionary.load('data.dict')
-corpus = corpora.MmCorpus('data.mm') # comes from the first tutorial, "From strings to vectors"
-# print(corpus)
+corpus = corpora.MmCorpus('data.mm')
 
 # lsi = models.LsiModel(corpus, id2word=dictionary, num_topics=200)
 # lsi = models.LdaModel(corpus, id2word=dictionary, num_topics=50, passes=5, eval_every=1)
 # lsi = models.LdaSeqModel(corpus, id2word=dictionary, num_topics=100, eval_every=10 passes=100)
-
-lsi = models.TfidfModel(corpus, id2word=dictionary)#, num_topics=120)
-# print ()
-# lsi.print_topics()
-# # exit()
+lsi = models.TfidfModel(corpus, id2word=dictionary)
 
 def get_sentence_similarity(sent_1, sent_2, model):
     sent_list_1 = sent_1.split()
@@ -44,7 +36,6 @@ def get_sentence_similarity(sent_1, sent_2, model):
             s1_use += 1
         except:
             pass
-        #     print ("err")
 
     for word in sent_list_2:
         try:
@@ -57,10 +48,8 @@ def get_sentence_similarity(sent_1, sent_2, model):
 
 
     x =  np.absolute(np.subtract(sent_sum_1 / s1_use, sent_sum_2 / s2_use))
-    # print (x)
     return sum(x)/len(x)
 
-# print ()
 doc = args.title
 vec_bow = dictionary.doc2bow(doc.lower().split())
 vec_lsi = lsi[vec_bow]
@@ -69,19 +58,13 @@ index = similarities.MatrixSimilarity(lsi[corpus])
 
 sims = index[vec_lsi]
 sims = sorted(enumerate(sims), key=lambda item: -item[1])
-# print (sims)
 model = models.Word2Vec.load("model.w2v")
 top = sims[:10]
 vals = []
 for i in top:
-    # print (i[0])
     full = df['jobpost'][i[0]]
     title = df['Title'][i[0]]
-    # print (text)
-    # print (get_sentence_similarity(text, doc, model))
-    # exit()
     # vals.append([model.wmdistance(doc, text), i[0]])
-    # print (text)
     vals.append([(get_sentence_similarity(full, doc, model) + get_sentence_similarity(title, doc, model)) / 2, i[0]])
 
 sims = sorted(vals, key=lambda item: item[0])
@@ -94,5 +77,3 @@ for i in sims[:10]:
     c += 1
     print ("")
     print ("")
-
-print (df['jobpost'][14])
