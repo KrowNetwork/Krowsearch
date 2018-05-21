4b65be73513873a91776917218cf4ac63aa8276f
unknown <siegel.tucker@gmail.com>
2018-05-21 16:17:51 -0400

    updates

--- create_w2v.py
+++ create_w2v.py
@@ -5,7 +5,22 @@ import numpy as np
 import argparse
 from datetime import datetime, date
 import dateparser
+import re
 
+df = pd.read_csv('datasets/data.csv')
+df.drop_duplicates()
+df.reset_index(drop=True)
+
+np.random.shuffle(df.values)
+documents = df['jobdescription']
+
+with open("assets/stopwords.txt", "r") as f:
+    stoplist = f.readlines()
+    for word in range(len(stoplist)):
+        stoplist[word] = stoplist[word].replace("\n", "")
+
+secondary_stoplist = "\\ / . -".split()
+texts_train = [[word for word in re.split(r'\s+|[,;.-/\\?.]\s*', str(document).lower()) if word not in stoplist] for document in documents]
 
 dictionary = corpora.Dictionary.load('data/data.dict')
 corpus = corpora.MmCorpus('data/data.mm')
--- process.py
+++ process.py
@@ -8,18 +8,12 @@ from sklearn.cluster import KMeans, MeanShift
 import re
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-df = pd.read_csv('datasets/data.csv')
+df = pd.read_csv('datasets/data2.csv')
 df.drop_duplicates()
 df.reset_index(drop=True)
 
-df2 = pd.read_csv('datasets/data2.csv')
-df2.drop_duplicates()
-df2.reset_index(drop=True)
-
 np.random.shuffle(df.values)
-np.random.shuffle(df2.values)
-documents = df['jobdescription']
-doc2 = df2['description']
+documents = df['description']
 
 with open("assets/stopwords.txt", "r") as f:
     stoplist = f.readlines()
@@ -27,8 +21,7 @@ with open("assets/stopwords.txt", "r") as f:
         stoplist[word] = stoplist[word].replace("\n", "")
 
 secondary_stoplist = "\\ / . -".split()
-texts_train = [[word for word in re.split(r'\s+|[,;.-/\\?.]\s*', str(document).lower()) if word not in stoplist] for document in documents]
-texts_save = [[word for word in re.split(r'\s+|[,;.-/\\?.]\s*', str(doc).lower()) if word not in stoplist] for doc in doc2]
+texts_save = [[word for word in re.split(r'\s+|[,;.-/\\?.]\s*', str(doc).lower()) if word not in stoplist] for doc in documents]
 
 dictionary = corpora.Dictionary(texts_save)
 dictionary.save('data/data.dict')
