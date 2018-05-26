fcf747fa0eeaa46f9ff7594c229f8779f39c7e84
unknown <siegel.tucker@gmail.com>
2018-05-26 17:31:50 -0400

    updages

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ var fs = require('fs');
 var os = require("os")
 var path = require("path")
 var cluster = require("cluster")
-
+var utf8 = require('utf8');
 var num_cpus = os.cpus().length;
-
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
+
+var current_page = 1
+var full_res = ""
 
 if (cluster.isMaster) {
   console.log(`Master ${process.pid} is running`);
@@ -22,29 +24,34 @@ if (cluster.isMaster) {
 
 } else {
 
-  app.engine('html', require('ejs').renderFile);
+  // app.engine('html', require('ejs').renderFile);
+  app.set('view engine', 'ejs')
   app.use(bodyParser.urlencoded({ extended: false }))
   app.use(bodyParser.json());
 
-  async function search(req, res, callback) {
+  async function search(req, res, page, callback) {
     var python = spawn('python', ['search.py'], {detached: true});
     var query = req.query.q;
+    // current_page += 1;
     return new Promise(function(resolve, reject){
 
       python.stdout.on('data', async (chunk) => {
         chunk = chunk.toString().split("'").join('"');
         results = JSON.parse(chunk);
+        json_res = results;
         // console.log(results);
         var data = ""
-        var input = results["10"] + " "
-        for (var i = 9; i >= 0; i --){
+        var results_num = page * 10 - 1
+        // console.log(results_num)
+        var input = results[results_num.toString()] + " "
+        for (var i = results_num - 1; i >= results_num - 10; i --){
           // console.log()
           input += results[i.toString()] + " "
         }
         // console.log(input)
          await process_ID(input)
           .then(function(result){
-            resolve(result.toString())
+            resolve(result.toString(), json_res)
           })
 
         // console.log(data);
@@ -55,6 +62,8 @@ if (cluster.isMaster) {
     })
 
   }
+
+
 
   function process_ID(jobID) {
     // var send = results[i.toString()]
@@ -75,21 +84,28 @@ if (cluster.isMaster) {
   }
 
   app.get("/", function(req, res) {
-    res.render("index.html")
+    res.render("index", {results: null})
   });
   app.get("/search", async (req, res, next) => {
-    res.writeHead(200,{"Content-Type" : "text/html"});
-    await search(req, res)
-      .then(function (result){
-        res.write(result)
-        res.end()
-      })
+    // res.writeHead(200,{"Content-Type" : "text/html"});
+    var page = req.query.page;
+    if (page === undefined) {
+      page = 1
+    }
+
+      // page = 1
+    await search(req, res, page)
+    .then(function (result, json_res){
+      result = utf8.encode(result)
+      res.render("index", {results: result})
+      full_json = json_res
+      // res.end()
+    })
+
+    
+
+
       return next()
-
-
-    // res.end()
-
-
 
   });
   app.listen(port, function (err) {
