b2c8f51ebfbb8838ef0ea157024e977f632ccd37
unknown <siegel.tucker@gmail.com>
2018-05-31 16:51:01 -0400

    test

--- server.js
+++ server.js
@@ -52,6 +52,11 @@ if (cluster.isMaster) {
   app.set('view engine', 'ejs')
   app.use(bodyParser.urlencoded({ extended: false }))
   app.use(bodyParser.json());
+  app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  });
 
   app.get("/search", async (req, res, next) => {
     // res.writeHead(200,{"Content-Type" : "text/html"});
