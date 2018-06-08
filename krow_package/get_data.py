abeb970be2d7aa778822f5e63da8b79fa2677810
unknown <siegel.tucker@gmail.com>
2018-06-07 20:36:26 -0400

    fix 'b' bug

--- krow_package/get_data.py
+++ krow_package/get_data.py
@@ -7,7 +7,7 @@ import requests
 r = requests.get("http://18.220.46.51:3000/api/queries/GetAvailableJobs")
 n = {}
 for i in r.json():
-    x = json.dumps(i).encode('utf8')
+    x = json.dumps(i)#.encode('utf8')
     # i = json.loads(i)
     n[i['jobID']] = x
 
@@ -18,5 +18,5 @@ while True:
     for i in id.split():
         x += (str(n[i]) + ",")
 
-    sys.stdout.write(x[:-1])
+    sys.stdout.write(str(x[:-1]))#.encode( "utf-8"))
     sys.stdout.flush()
