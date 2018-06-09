802d86f304aee94cbbc0a4ae34a4c5f49cef110c
unknown <siegel.tucker@gmail.com>
2018-06-09 00:29:26 -0400

    asdfasdfasdfasdfadfs

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ if (cluster.isMaster) {
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Content-Length", "262144");
+    // res.header("Content-Length", "262144");
     next();
   });
 
