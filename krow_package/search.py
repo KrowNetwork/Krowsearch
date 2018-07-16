20d77becae71f2baf960a723d28e92dc1d672a2a
unknown <siegel.tucker@gmail.com>
2018-07-15 22:36:51 -0400

    updates

--- krow_package/search.py
+++ krow_package/search.py
@@ -130,7 +130,7 @@ async def search(term, sort_type):
     else:
         if args.t:
             loop_now = time.time()
-        async for i in iterate_data(top[:10]):
+        async for i in iterate_data(top[:100]):
             vals.append([await calc(i), i[0]])
         if args.t:
             loop_now = time.time() - loop_now
@@ -139,7 +139,7 @@ async def search(term, sort_type):
 
     data = str(r[sims[0][1]]["jobID"]) + " "
 
-    for i in range(1, len(sims)):
+    for i in range(1, 10):
         data += str(r[sims[i][1]]["jobID"]) + " "
 
     print (data[:-1])
