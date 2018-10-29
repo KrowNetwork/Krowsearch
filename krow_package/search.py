99096f74e1264446baf8d4958dc1fd0337515b1c
unknown <siegel.tucker@gmail.com>
2018-10-29 15:41:46 -0400

    search engine update

--- krow_package/search.py
+++ krow_package/search.py
@@ -9,19 +9,24 @@ headers = {'x-api-key': 'qLBrEwIv690nAbMfVHB965WC3KfoC1VpvkBjDUiBfVOG5mTzlUlwkck
 r = requests.get("http://18.220.46.51:3000/api/queries/GetUsers", headers=headers)
 data = {}
 for i in r.json():
-    data[i["firstName"] + " " + i["lastName"]] = i['applicantID']
-# print (i)
+    data[ i['applicantID']] = i["firstName"] + " " + i["lastName"]
+    # print (i['firstName'] + " " + i["lastName"])
+    # print (i['applicantID'])
 
 def search(name):
     name = name.split()
     res = []
     stop = False
     for i in data:
+        a = data[i]
+        # print (i)
         for n in name:
-            if n in i.lower():
-                res.append([i, data[i], similar(i, " ".join(name))])
-                break
+            if n in a.lower():
+                res.append([a, i, similar(a.lower(), " ".join(name).lower())])
+                
             
+    # for i in res:
+        # print (i)
 
     res.sort(key=lambda x: x[2])
     res = res[::-1]
